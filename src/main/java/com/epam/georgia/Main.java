8d28a5b9989c55033dca1b49711319ef10f0e7fe
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2022-03-30 21:22:26 +0400

    fix maven config

--- src/main/java/com/epam/georgia/Main.java
+++ src/main/java/com/epam/georgia/Main.java
@@ -1,4 +1,14 @@
 package com.epam.georgia;
 
 public class Main {
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("TEST: " + args[0]);
+        } else {
+            System.out.println("TEST: " + args.length);
+        }
+
+    }
+
 }
